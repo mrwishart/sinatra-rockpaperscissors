0517db6f619a970eb4aa8868789fcb7f170b9ca4
Paul Wishart <misterwishart@gmail.com>
2018-11-06 18:31:10 +0000

    Created Game class. Tested successfully. Added option for 1-player game against the computer

--- controller.rb
+++ controller.rb
@@ -1,23 +1,13 @@
 require 'sinatra'
 require 'sinatra/contrib/all' if development?
+require_relative './models/game'
 
 get '/:move1/:move2' do
-  move1 = params[:move1].to_s.downcase
-  move2 = params[:move2].to_s.downcase
+  game = Game.new(params)
+  return game.result()
+end
 
-  win_conditions = {
-    "scissors" => "paper",
-    "paper" => "rock",
-    "rock" => "scissors"
-  }
-
-  if move1 == move2
-    return "Draw!"
-  elsif win_conditions[move1] == move2
-    return "Player 1 wins with #{move1}"
-  elsif move1 == win_conditions[move2]
-    return "Player 2 wins with #{move2}"
-  else
-    return "Pick actual options, ya dick!"
-  end
+get '/:move1/' do
+  game = Game.new(params)
+  return game.result()
 end
