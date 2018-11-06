61be5daad195ce89d9273861c42d06af07a4ad65
Paul Wishart <misterwishart@gmail.com>
2018-11-06 19:31:11 +0000

    Added home page. Altered layout. Changed controller to link to home. Modified erb files to allow for user to click on their choices

--- controller.rb
+++ controller.rb
@@ -2,14 +2,23 @@ require 'sinatra'
 require 'sinatra/contrib/all' if development?
 require_relative './models/game'
 
-get '/:move1/:move2' do
+get '/r/:move1/:move2' do
   game = Game.new(params)
   @result = game.result()
   erb(:result)
 end
 
-get '/:move1' do
+get '/r/:move1' do
   game = Game.new(params)
   @result = game.result()
   erb(:result)
+end
+
+get '/:move1/choosesecond' do
+  @move1 = params[:move1]
+  erb(:choosesecond)
+end
+
+get '/' do
+  erb(:home)
 end
